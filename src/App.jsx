2ca267eb41ec13c198b033b586be32424abfbf70
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-08 22:35:33 -0700

    adde dpaths

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import {HashRoutr, Route, Routes, Link, HashRouter} from "react-router-dom"
 import './App.css'
 
+import {home} from './home.js'
+import {contact} from './contact.js'
+import {about} from './about.js'
+import { projects } from '../projects.js'
+
 function App() {
 
 
@@ -16,19 +21,19 @@ function App() {
             <Link to="About">About</Link>
           </li>
           <li>
-            <Link to ="/Projects">Projects</Link>
+            <Link to ="/projects">Projects</Link>
           </li>
           <li>
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
-      
+
       <Routes>
-        <Route path path='s' element ={< >} />
-        <Route path path='s' element ={< >} />
-        <Route path path='s' element ={< >} />
-        <Route path path='s' element ={< >} />
+        <Roout   path='/' element ={<home />} />
+        <Route   path='/about' element ={<about />} />
+        <Route   path='/projects' element ={<projects />} />
+        <Route   path='/contact' element ={<contact />} />
       </Routes>
     </HashRouter>
     

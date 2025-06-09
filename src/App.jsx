1a3d7db8a13ffa35b1536b269d04ac3f81fae76b
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-08 22:17:33 -0700

    added routes for redo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {HashRoutr, Route, Routes, Link, HashRouter} from "react-router-dom"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HashRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" >Home</link>
+          </li>
+          <li>
+            <Link to="About">About</link>
+          </li>
+          <li>
+            <Link to ="/Projects">Projects</link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path path='s' element ={< >} />
+        <Route path path='s' element ={< >} />
+        <Route path path='s' element ={< >} />
+        <Route path path='s' element ={< >} />
+      </Routes>
+    </HashRouter>
+    
   )
 }
 

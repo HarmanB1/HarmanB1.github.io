6d183a7b9593fb36be29b2172d652ce042c008d7
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-08 23:32:31 -0700

    added sytling

---
+++ src/home.jsx
@@ -0,0 +1,35 @@
+export const Home = () => {
+    return(
+            <section id="overview">
+
+            
+            <h1>Bio</h1>
+            
+            <p>Hi, I’m Harman Bhogal, a second-year Computer Science student at Simon Fraser University (SFU). My passion for coding and problem-solving has been a key part of my academic journey. While I’m skilled in various programming languages, C++ is my preferred method of solving problems. </p>
+
+            <p> Alongside my studies, I work as a math tutor for younger children. Teaching has been an incredibly rewarding experience and allows me to gain a deeper understanding of my local community. </p>
+            
+            <p> My goal is to eventually become a software engineer, and I’m eager to continue developing my coding abilities and gaining real-world experience. I’m particularly interested in the developing field of Artificial Intelligence. </p>
+            
+            <p> Outside of school and tutoring, I enjoy going to the gym. I also love playing video games and watching movies. </p>
+            
+            
+            <h2>What I'm Currently Working On</h2>
+            <p>
+            I am currently learning Java for mobile app development, C# for Windows applications, and experimenting with OpenCV, TensorFlow, and MediaPipe
+            for AI and camera tracking experimentation. 
+            </p>
+            <p>
+            Goal: After learning these applications, specifically the Python libraries, my eventual goal is to build an application that unlocks a device—specifically a Windows laptop—through user gestures.
+            </p>
+
+            
+            <h2>Interest</h2>
+            <p>Movies: <a href="https://boxd.it/2xOqX" >Letterboxd</a></p>
+            
+            <p>Chess: <a href="https://www.chess.com/home">Chess</a></p>
+        
+            
+        </section>
+    )
+}

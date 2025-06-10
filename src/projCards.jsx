c2660917c6c3667e2d8821728a60e711fbb65bed
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-09 18:44:58 -0700

    fixed backtick for interpolation

--- src/projCards.jsx
+++ src/projCards.jsx
@@ -9,12 +9,12 @@ export const Card = ({title, descrip, image, status, link}) =>{
             role="button"
             aria-pressed="false"
         >
-            <div className="cardImg" aria-label={'Image for ${title'}>
+            <div className="cardImg" aria-label={`Image for ${title}`}>
                 {image ? <img src={image} alt={title}/>:null}
             </div>
             <div className="descrip">
                 <h3>{title}</h3>
-                <p className={'status: ${status}'}>{status} </p>
+                <p className={`status: ${status}`}>{status} </p>
                 <p>{descriptioin}</p>
             </div>
         </div>

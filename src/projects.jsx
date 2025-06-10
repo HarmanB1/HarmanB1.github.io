ced1bf2d04880f2b6180502d6dae2ec0c425ca6f
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-09 22:25:10 -0700

    added finished projec descriptions"

--- src/projects.jsx
+++ src/projects.jsx
@@ -6,27 +6,27 @@ import { Rotation } from "./Rotation.jsx";
 const cards = [
   <Card
     key={1}
-    title="Alpha"
-    descrip="React project"
+    title="Terminal Editor"
+    descrip="A terminal-based text editor built in C++ with ncurses, featuring mouse support, file operations, and customizable themes."
     image=""
     status="finished"
-    link="https://example.com/alpha"
+    link="https://github.com/HarmanB1/Editor"
   />,
   <Card
     key={2}
-    title="Beta"
-    descrip="ML project"
+    title="Workout Recommendation Website"
+    descrip="Responsive React web app offering personalized workouts, weather-based clothing suggestions, and real-time integration with weather and exercise APIs."
     image=""
-    status="working"
-    link="https://example.com/beta"
+    status="finished"
+    link="https://gitfit.works/"
   />,
   <Card
     key={3}
-    title="Gamma"
-    descrip="Flutter app"
+    title="Personal Website"
+    descrip="Clean, responsive personal portfolio website developed initally using HTML and CSS then rebuilt in React, focusing on usability, maintainability, and modern design."
     image=""
     status="finished"
-    link="https://example.com/gamma"
+    link="https://harmanb1.github.io"
   />,
 ];
 

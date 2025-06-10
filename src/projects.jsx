0cd1f6be79a2ae133cf31863cdf2670705be1f6d
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-09 22:28:58 -0700

    added images to projects"

--- src/projects.jsx
+++ src/projects.jsx
@@ -8,7 +8,7 @@ const cards = [
     key={1}
     title="Terminal Editor"
     descrip="A terminal-based text editor built in C++ with ncurses, featuring mouse support, file operations, and customizable themes."
-    image=""
+    image="./src/imgs/Screenshot 2025-06-03 at 7.32.48 PM.png"
     status="finished"
     link="https://github.com/HarmanB1/Editor"
   />,
@@ -16,7 +16,7 @@ const cards = [
     key={2}
     title="Workout Recommendation Website"
     descrip="Responsive React web app offering personalized workouts, weather-based clothing suggestions, and real-time integration with weather and exercise APIs."
-    image=""
+    image="./src/imgs/Screenshot 2025-06-09 at 10.24.50 PM.png"
     status="finished"
     link="https://gitfit.works/"
   />,
@@ -24,7 +24,7 @@ const cards = [
     key={3}
     title="Personal Website"
     descrip="Clean, responsive personal portfolio website developed initally using HTML and CSS then rebuilt in React, focusing on usability, maintainability, and modern design."
-    image=""
+    image="./src/imgs/Screenshot 2025-06-09 at 10.24.29 PM.png"
     status="finished"
     link="https://harmanb1.github.io"
   />,
